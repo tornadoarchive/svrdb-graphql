6e6fd79119f2e203a464dc470dba85bc315c2911
wxmann <jim.z.tang@gmail.com>
2021-11-30 21:55:35 -0800

    copy over fips fixes

--- seed.py
+++ seed.py
@@ -163,9 +163,11 @@ def _correct_tor_records(df):
         # if no dup exists because subsequent SPC files were fixed, we're good
         pass
 
-    # mislabeled duplicate om's -- found by @tsupinie
+    # mislabeled/duplicate om's -- found by @tsupinie
     df.loc[(df.om == 9999) & (df.yr == 1995) & (df.st == 'IA'), 'om'] = 9998
     df.loc[(df.om == 576455) & (df.yr == 2015) & (df.st == 'NE'), 'om'] = 576454
+    df.loc[(df.om == 265) & (df.yr == 1953) & (df.st == 'IA'), 'om'] = 263
+    df.loc[(df.om == 456) & (df.yr == 1961) & (df.st == 'SD'), 'om'] = 454
 
     # missing state segment from a KS-NE tornado in Mar 1993
     # see https://www.ncdc.noaa.gov/stormevents/eventdetails.jsp?id=10326096 and
@@ -189,6 +191,31 @@ def _correct_tor_records(df):
         par_tornado_1993,
         ne_segment_1993
     ], ignore_index=True)  # this avoids pandas.errors.InvalidIndexError
+
+    # fix fips/counties, credit goes to @tsupinie for these fixes
+    fips_fixes = {
+        (46, 131): 71,  # Washabaugh County, SD merged with Jackson County, SD
+        (12, 25): 86,  # Dade County, FL renamed Miami-Dade County, FL
+        (13, 597): 197,  # Typo on the FIPS code for Marion County, GA?
+        (51, 39): 37,  # Typo on the FIPS code for Charlotte County, VA?
+        (27, 2): 3,  # Typo on the FIPS code for Anoka County, MN?
+        (51, 123): 800,  # Suffolk City, VA replaced Nansemond County, VA
+        (46, 1): 3,  # Typo on the FIPS code for Aurora County, SD?
+        (29, 677): 77,  # Typo on the FIPS code for Greene County, MO?
+        (21, 22): 33,  # Typo on the FIPS code for Caldwell County, KY?
+        (42, 159): 15,  # Typo on the FIPS code for Bradford County, PA?
+        (72, 8): 5,  # Typo on the FIPS code for Aguadilla, PR?
+        (46, 113): 102,  # Shannon County, SD became Ogalala Lakota County
+        (2, 155): 50,  # Old code for Bethel Census Area?
+        (2, 181): 13  # Old code for Aleutians East Borough?
+    }
+
+    for fips, replacement in fips_fixes.items():
+        stf, ctf = fips
+        for county_col in ('f1', 'f2', 'f3', 'f4'):
+            df.loc[(df.stf == stf) & (df[county_col] == ctf), county_col] = replacement
+
+    df.loc[(df.yr == 1966) & (df.om.isin([13, 14])), 'f1'] = [83, 81]
 
     return df
 

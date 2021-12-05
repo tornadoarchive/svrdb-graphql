ca32248174723ccb7b6d0b4d1391725ec1c37def
wxmann <jim.z.tang@gmail.com>
2021-12-05 12:22:25 -0700

    mount data dir to docker container specific path

--- seeding/datasrcs.py
+++ seeding/datasrcs.py
@@ -2,14 +2,20 @@ import os
 
 from decouple import config
 
-datadir = config('DATA_FILE_DIR')
+
+def get_datadir():
+    # TODO: this will only work locally mounting data dir to ../data
+    # How will this work in non-local environments?
+    current_dir = os.path.dirname(__file__)
+    parent_dir = os.path.dirname(current_dir)
+    return os.path.join(parent_dir, 'data')
 
 
 class _Files:
-    SPC_TOR = os.path.join(datadir, config('SPC_TOR_FILE'))
-    SPC_WIND = os.path.join(datadir, config('SPC_WIND_FILE'))
-    SPC_HAIL = os.path.join(datadir, config('SPC_HAIL_FILE'))
-    US_COUNTIES = os.path.join(datadir, config('US_COUNTY_FILE'))
+    SPC_TOR = os.path.join(get_datadir(), config('SPC_TOR_FILE'))
+    SPC_WIND = os.path.join(get_datadir(), config('SPC_WIND_FILE'))
+    SPC_HAIL = os.path.join(get_datadir(), config('SPC_HAIL_FILE'))
+    US_COUNTIES = os.path.join(get_datadir(), config('US_COUNTY_FILE'))
 
 
 files = _Files

89f3bf2e72fca940e621309980bc8c9c21641cf3
wxmann <jim.z.tang@gmail.com>
2021-12-21 00:43:56 -0800

    switch from debug server to Fastapi

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import strawberry
+from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
 
 from svrdb.types import Query
 
-schema = strawberry.Schema(query=Query)
+schema = strawberry.Schema(Query)
+
+graphql_app = GraphQLRouter(schema)
+
+app = FastAPI()
+app.include_router(graphql_app, prefix="/graphql")

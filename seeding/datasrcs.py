d10db5bd0dd1b5756b529255dd7045bf2a366d6f
wxmann <jim.z.tang@gmail.com>
2021-12-24 01:34:01 -0800

    run seed.py against remote database

--- seeding/datasrcs.py
+++ seeding/datasrcs.py
@@ -4,8 +4,10 @@ from decouple import config
 
 
 def get_datadir():
-    # TODO: this will only work locally mounting data dir to ../data
-    # How will this work in non-local environments?
+    # if loading non-locally, use DATA_FILE_DIR directly, else use docker-mounted volume
+    if config('RUN_REMOTE', cast=bool, default=False):
+        return config('DATA_FILE_DIR')
+
     current_dir = os.path.dirname(__file__)
     parent_dir = os.path.dirname(current_dir)
     return os.path.join(parent_dir, 'data')

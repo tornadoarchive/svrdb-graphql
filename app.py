3b9f12c4e5137c84c67c02a2569b206858a504b7
wxmann <jim.z.tang@gmail.com>
2022-01-31 18:00:18 -0800

    enable CORS temporarily

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import strawberry
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from strawberry.fastapi import GraphQLRouter
 
 from svrdb.types import Query
@@ -10,3 +11,13 @@ graphql_app = GraphQLRouter(schema)
 
 app = FastAPI()
 app.include_router(graphql_app, prefix="/graphql")
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
